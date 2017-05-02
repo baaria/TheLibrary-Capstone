cff2fb82d1eef1762271f4201f7735a7ebe15f4f
baaria <baaria@nyu.edu>
2017-05-02 15:07:30 +0800

    adding scripts

---
+++ ClownCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownCollider : MonoBehaviour {
+    public Collider playa;
+    public GameObject clown;
+    public GameObject noFace;
+
+    // Use this for initialization
+    void Start()
+    {
+        clown.SetActive(false);
+        noFace.SetActive(true);
+    }
+
+	void OnTriggerEnter(Collider other)
+    {
+        if(GetComponent<Collider>() == playa)
+        {
+            clown.SetActive(true);
+            noFace.SetActive(false);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        clown.SetActive(false);
+        noFace.SetActive(true);
+    }
+}
---
+++ scenePortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scenePortal : MonoBehaviour {
+
+    public Collider playerCollider1;
+    public GameObject rig;
+    public string sceneToLoad;
+    public GameObject spawnPos;
+    public bool spawnPosLoc;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+    {
+        if(other == playerCollider1)
+        {
+            SteamVR_LoadLevel.Begin(sceneToLoad);
+            if(spawnPosLoc)
+            {
+                rig.transform.position = spawnPos.transform.position;
+            }
+            
+            
+
+        }
+    }
+}
